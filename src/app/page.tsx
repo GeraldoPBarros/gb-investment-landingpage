d8bcfb755667d610dc22a45e57d2606be275e690
BarrosNB <69353700+BarrosNB@users.noreply.github.com>
2025-06-07 16:38:11 -0300

    project structure.

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+import { ArrowRight } from "lucide-react";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between bg-black">
+      {/* Hero Section */}
+      
+      <p>Hello</p>
+      
+    </main>
+  );
+}
